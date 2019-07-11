aec532026d9982f15716d5b327b1f06351b85861
fv17 <forest.valley0417@gmail.com>
2019-07-11 13:43:17 +0900

    feat: 【完成】勝者判定機能を追加

--- src/index.js
+++ src/index.js
@@ -22,7 +22,12 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    if(this.state.xIsNext) {
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    if (this.state.xIsNext) {
       squares[i] = 'X';
     } else {
       squares[i] = '◯';
@@ -44,7 +49,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : '◯');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : '◯');
+    }
 
     return (
       <div>
@@ -83,6 +94,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
